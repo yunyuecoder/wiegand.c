31d969badeee654b148cf9003bed2bb3234236d3
eric <chenguanghai@mail.sf-bamboo.cn>
2016-09-29 17:38:44 +0800

    fix bug

--- wiegand.c
+++ wiegand.c
@@ -130,17 +130,20 @@ void send_intent(int bits , char data[],char index[])
     type = -1;
   }else if(bits == 34){
     type = -2;
-  }else {
+  }
+  if (type == -3){
     return;
+  }else{
+    int off=snprintf(intent, 4096, "am broadcast -a com.sfz.checkCardIDMessage -ei index %s --ei type %d --es cardId ", index, type);
+    int szPos;
+    for (szPos = 1; szPos <= bytes; szPos++) {
+      off += snprintf(intent + off, 4096 - off, "%02x" ,(unsigned char)data[szPos]);
+    }
+    off += snprintf(intent + off, 4096 - off, "\n");
+    //system(intent);
+    printf("%s\n",intent);
   }
-  int off=snprintf(intent, 4096, "am broadcast -a com.sfz.checkCardIDMessage -ei index %s --ei type %d --es cardId ", index, type);
-  int szPos;
-  for (szPos = 1; szPos <= bytes; szPos++) {
-    off += snprintf(intent + off, 4096 - off, "%02x" ,(unsigned char)data[szPos]);
-  }
-  off += snprintf(intent + off, 4096 - off, "\n");
-  //system(intent);
-  printf("%s\n",intent);
+
 }
 
 void main(int argc,char *argv[]) {
